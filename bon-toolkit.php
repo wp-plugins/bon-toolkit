b8bdab1c8e69e23fd77f768b4270c13c9f0f3f9c
nackle2k10 <nackle2k10@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-01 19:45:35 +0000

    Update Readme
    git-svn-id: https://plugins.svn.wordpress.org/bon-toolkit/trunk@781020 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bon-toolkit.php
+++ bon-toolkit.php
@@ -3,7 +3,7 @@
 Plugin Name: Bon Toolkit
 Plugin URI: http://bonfirelab.com
 Description: Various widgets, shortcodes and elements for your WordPress site.
-Version: 1.0.9
+Version: 1.1.0
 Author: Hermanto Lim
 Author URI: http://www.bonfirelab.com
 */
@@ -17,7 +17,7 @@ if ( ! class_exists( 'BON_Toolkit' ) ) {
 		/**
 		 * @var string
 		 */
-		public $version = '1.0.9';
+		public $version = '1.1.0';
 
 		/**
 		 * @var string
